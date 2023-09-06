1be1a3c3ae5f0a78fc37b902695251deae19c121
ruansbueno <ruansbueno@gmail.com>
2023-09-06 16:53:44 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+const bodyparser = require('body-parser')
+
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('views',path.join(__dirname,'/views'))
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
+
+let tarefas = []
+
+app.get('/',(req,res)=>{
+	res.render('index', {tarefasList:tarefas})
+})
+
+app.post('/',(req,res)=>{
+	tarefas.push(req.body.tarefa)
+	res.render('index', {tarefasList:tarefas})
+})
+
+app.get('/deletar/:id', (req, res)=>{
+	tarefas = tarefas.filter((val,index)=>{
+		if(index != req.params.id){
+			return val;
+		}
+	})
+
+	res.redirect('/')
+})
+
+app.listen(port,()=>{
+	console.log('rodando')
+})
+
+//parei na aula "desenvolvendo projeto prático 1/2 curso de nodejs 8 min"
